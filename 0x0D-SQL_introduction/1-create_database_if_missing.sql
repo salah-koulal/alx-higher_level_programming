84d5447105a83f5d5d057067f7b45281979a47c6
salah-koulal <salah.rajawi289@gmail.com>
2023-10-17 23:54:04 +0100

    SQL-INTRO-updated

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creating a database named hbtn_0c_0 in a MySQL server
+-- Using a CREATE and IF condition
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+--  deletes the database hbtn_0c_0 in your MySQL server.
+-- Using the DROP command and an IF condition
+DROP DATABASE IF EXISTS hbtn_0c_0;
