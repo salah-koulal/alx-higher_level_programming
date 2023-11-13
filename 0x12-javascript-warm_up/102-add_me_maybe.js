a9a80f1d6eefd0be4a103c0f642adba96388d1d5
salah-koulal <salah.rajawi289@gmail.com>
2023-11-13 23:40:36 +0100

    Fix an error

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,10 @@
-o#!/usr/bin/node
+#!/usr/bin/node
 module.exports = {
   addMeMaybe: function (nb, func) {
     nb++;
     func(nb);
   }
-};
+};:let arrayIndex = array.length
+while (arrayIndex--) {
+  
+}
