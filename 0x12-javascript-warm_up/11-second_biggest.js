8853a5b4462218e117214ee8e6e11bf61fe9e334
salah-koulal <salah.rajawi289@gmail.com>
2023-11-13 22:52:20 +0100

    BiggestJs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+function secondBiggest (list) {
+  let first = -Infinity;
+  let second = -Infinity;
+  for (const i of list) {
+    if (!isNaN(Number(i))) {
+      if (first < Number(i)) {
+        second = first;
+        first = Number(i);
+      } else if (second < Number(i) && first !== Number(i)) {
+        second = Number(i);
+      }
+    }
+  }
+  return second;
+}
+const args = process.argv.slice(2);
+if (!args[0] || args.length === 1) {
+  console.log(0);
+} else {
+  console.log(secondBiggest(args));
+}
