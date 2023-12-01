a43f49f3c04a274a7dc67120e53375ea2f8542b6
salah-koulal <salah.rajawi289@gmail.com>
2023-12-01 22:03:08 +0100

    hbtn-Status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
