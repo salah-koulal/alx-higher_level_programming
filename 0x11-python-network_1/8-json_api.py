9aa497cb2e429eef20ca5f7e0d3ea27c6dfcee0e
salah-koulal <salah.rajawi289@gmail.com>
2023-12-01 22:18:58 +0100

    JsonApi

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        content = response.json()
+        content_id = content.get("id", "")
+        content_name = content.get("name", "")
+        if content_id and content_name:
+            print("[{}] {}".format(content_id, content_name))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
