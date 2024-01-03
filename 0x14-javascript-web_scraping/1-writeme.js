26d87f058760cbd1ae7425285dbb666153b13233
salah-koulal <salah.rajawi289@gmail.com>
2024-01-03 22:40:18 +0100

    done1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const file = process.argv[2];
+const string = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(file, string, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
